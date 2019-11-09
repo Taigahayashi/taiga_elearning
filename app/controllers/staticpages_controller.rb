bd839a6002f8482f062764aad94c89132a2db283
Taigahayashi <yaku0625@gmail.com>
2019-11-09 17:19:59 +0800

    make-static-pages

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,8 +1,4 @@
 class StaticpagesController < ApplicationController
     def home
-        #if logged_in?
-            #@micropost = Micropost.new
-           # @microposts = current_user.feed
-       # end    
     end
 end
