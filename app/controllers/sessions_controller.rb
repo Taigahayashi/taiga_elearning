7543eac0ef226be4f6109edd65c2edadb7f1b966
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-12 12:37:13 +0800

    create login logout (#4)
    * create login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email])
 
     if user && user.authenticate(params[:session][:password])
-      
+      log_in user
       flash[:info] = "Succesflly logged in."
       redirect_to root_url
     else
@@ -13,4 +14,10 @@ class SessionsController < ApplicationController
       flash[:danger] = "Invaild Credentions"
     end
   end
+  def destroy
+    log_out
+    flash[:info] = "Successflly logged out"
+    redirect_to root_url
+  end
+  
 end
