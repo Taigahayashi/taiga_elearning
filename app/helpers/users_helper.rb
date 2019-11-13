7245b7b97b20e5b99916b9db6e9ea64dfdf6ed8e
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-13 11:20:51 +0800

    create userlist (#6)
    * create userlist

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	def gravatar_for(user)
+		# Generate hex token
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name)
+  end
 end
