8a244e5c9e1609198b3c5ca5ccc0aef974fceadf
Taigahayashi <yaku0625@gmail.com>
2019-11-14 09:41:17 +0800

    create profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	def gravatar_for(user)
-		# Generate hex token
+		#Generate hex token
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name)
