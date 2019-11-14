612a3da5cf3aa87e305cbfced80075d34c8734f4
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-14 12:25:23 +0800

    create following (#8)
    * create following

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   delete '/logout', to:'sessions#destroy' 
+  resources :sessions
+  resources :users
+  resources :relationships,only:[:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
