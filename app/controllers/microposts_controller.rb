9a625708946785bef89d0807f800454a4be9c9d1
Taigahayashi <yaku0625@gmail.com>
2019-11-14 09:59:42 +0800

    fix indention

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,18 +1,18 @@
 class MicropostsController < ApplicationController
-    def create
-        @micropost = current_user.microposts.build(micropost_params)
-        if @micropost.save
-            flash[:success]= "Micropost Created"
-            redirect_to root_url
-        else
-            @microposts = current_user.microposts
-            render "users/home_feeds"
-        end
-    end
+	def create
+		@micropost = current_user.microposts.build(micropost_params)
+		if @micropost.save
+			flash[:success]= "Micropost Created"
+			redirect_to root_url
+		else
+			@microposts = current_user.microposts
+			render "users/home_feeds"
+		end
+	end
 
 
-    private
-        def micropost_params
-            params.require(:micropost).permit(:content)
-        end
+	private
+	def micropost_params
+		params.require(:micropost).permit(:content)
+	end
 end
