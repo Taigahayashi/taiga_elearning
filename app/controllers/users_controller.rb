cd10b3108ee1f5d218c22c9f322566f41fda11ef
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-14 10:01:50 +0800

    create profile (#7)
    * create profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def index
     @users = User.paginate(page: params[:page],per_page: 10)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
