350fb0603c6da2c938771b0b7fe0e627f4b0b943
Taigahayashi <yaku0625@gmail.com>
2019-11-18 11:09:29 +0800

    fix URL

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :relationships,only:[:create,:destroy]
   namespace :admin do
     get'/home', to:'users#home'
-    get'/category', to:'users#index'
+    get'/users', to:'users#index'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
