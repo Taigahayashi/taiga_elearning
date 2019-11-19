4e96937d0cc8723d17365bd9b882061ba3fd3fc0
Taigahayashi <yaku0625@gmail.com>
2019-11-19 10:03:59 +0800

    create [layout]Category index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_034907) do
+ActiveRecord::Schema.define(version: 2019_11_19_011727) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.string "follower_id"
