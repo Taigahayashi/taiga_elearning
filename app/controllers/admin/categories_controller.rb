11042d918931e1aa0cf13c85aa9de4743601896d
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-20 12:27:40 +0800

    create category show page (#15)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -14,7 +14,11 @@ class Admin::CategoriesController < ApplicationController
 
 	def index
 		@categories = Category.paginate(page: params[:page],per_page: 10)
-	end
+  end
+
+  def show
+    @categories = Category.paginate(page: params[:page],per_page: 10)
+  end
 
 	def edit
 		@category = Category.find(params[:id])
