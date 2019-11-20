bf377f0ca4c22cec2bacaf3108711211a6f150c4
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-20 11:17:59 +0800

    create edit-delete function for Category (#14)
    * create edit-delete

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,17 +2,39 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
 	end
+
 	def create
     @category = Category.new(category_params)
     if @category.save
       redirect_to admin_categories_url
     else
-      render 'index'
+      render 'new'
     end
 	end
+
 	def index
 		@categories = Category.paginate(page: params[:page],per_page: 10)
 	end
+
+	def edit
+		@category = Category.find(params[:id])
+	end
+	def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    flash[:info] = "Successflly Removed category"
+    @category.destroy
+    redirect_to admin_categories_url
+  end
+	
 	private
     def category_params
       params.require(:category).permit(:title,:description)
