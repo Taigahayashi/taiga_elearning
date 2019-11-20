e9a74c2820d811f7b7ae55cfbd8e7cc2e61d5f67
Taigahayashi <yaku0625@gmail.com>
2019-11-20 12:22:49 +0800

    create category show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_011727) do
+ActiveRecord::Schema.define(version: 2019_11_20_033601) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2019_11_19_011727) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "words", force: :cascade do |t|
   end
 
 end
