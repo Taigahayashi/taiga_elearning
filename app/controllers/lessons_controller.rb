ad867cfd8fe78e83c9c665e4843ab21b3f7a08b8
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-28 12:10:07 +0800

    create activity-feed for Lesson (#20)
    * create activity-feed

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,19 @@
 class LessonsController < ApplicationController
   def create
-    @category = Category.find(params[:category_id])
-    @lesson = @category.lessons.build(user: current_user)
-
+    @lesson = Lesson.new(lesson_params)
     if @lesson.save
-	    redirect_to new_lesson_answer_url(@lesson)
+	    redirect_to new_lesson_answer_path(@lesson)
     end	
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    @category = Category.find(@lesson.category_id)
+  end
+
+  private
+
+  def lesson_params
+    params.permit(:category_id).merge(user_id: current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
   has_many :following,through: :active_relationships, source: :followed
   has_many :followers,through: :active_relationships, source: :follower
   has_many :lessons,dependent: :destroy
+  has_many :activities,dependent: :destroy
 
 
   #follow a user
