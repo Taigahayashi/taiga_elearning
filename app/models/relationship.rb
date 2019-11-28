0f56a0e60955d4b69ca1fff4f1cd0cd00987e49a
Taigahayashi <57470339+Taigahayashi@users.noreply.github.com>
2019-11-28 12:22:59 +0800

    acitivity-feed-follow (#21)

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,11 @@ class Relationship < ApplicationRecord
   validates :follower_id,presence: true
   validates :followed_id,presence: true
   has_one :activity,as: :action,dependent: :destroy
+
+  after_create :relationship_activity
+
+  private
+  def relationship_activity
+    create_activity(user: follower)
+  end
 end
